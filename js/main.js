34b5ffa43236949536971bdc4ed6e0aaa463c67c
Hellon Canella <helloncanella@gmail.com>
2015-10-26 13:57:04 -0200

    cursor is ok

--- js/main.js
+++ js/main.js
@@ -2,23 +2,27 @@
 
 'use strict';
 
-require(['jquery','plotter'], function($, Plotter){
+require(['jquery', 'plotter'], function($, Plotter) {
 
   var
-    xLimits = [-1000,1000],
-    yLimits = [-15,15];
+    xLimits = [-1000, 1000],
+    yLimits = [-15, 15];
 
-  var plotter = new Plotter(xLimits,yLimits);
+  var plotter = new Plotter(xLimits, yLimits);
 
-  (function insertPlotter(){
-    plotter.insertSVGframe().setAxes();
+  (function insertPlotter() {
+    plotter
+      .insertSVGframe()
+      .setAxes()
+      .setListeners();
 
-    $(window).resize(function(){
+    $(window).resize(function() {
       insertPlotter();
     });
 
   })();
 
   console.log(window);
+
 
 });
--- js/plotter.js
+++ js/plotter.js
@@ -1,11 +1,13 @@
-/*globals define*/
+/*globals define, window*/
 /*jshint -W098, -W003*/
 
 'use strict';
 
 define(['d3', 'jquery'], function(d3, $) {
 
-  var padding = 40;
+  var
+    padding = 40,
+    select = d3.select;
 
   function Plotter(xLimits, yLimits) {
     this.xLimits = xLimits;
@@ -39,29 +41,73 @@ define(['d3', 'jquery'], function(d3, $) {
       xScale = d3.scale.linear().domain([this.xLimits[0], this.xLimits[1]]).range([0, width]),
       yScale = d3.scale.linear().domain([this.yLimits[0], this.yLimits[1]]).range([height, 0]);
 
-    console.log(width,height);
-
     var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
     var yAxis = d3.svg.axis().scale(yScale).orient('left');
 
-    this.frame
+    this.xAxis = this.frame
       .append('g')
+      .attr('height', '21px')
       .attr({
-        class: 'axis',
-        transform: 'translate(0,' + (height /2) + ')'
+        class: 'axis X',
+        transform: 'translate(0,' + (height / 2) + ')',
+        height: '21px'
       })
       .call(xAxis);
 
-    this.frame
+    this.yAxis = this.frame
       .append('g')
+      .attr('width', '21px')
       .attr({
-        class: 'axis',
-        transform: 'translate(' + (width /2) + ',0)'
+        class: 'axis Y',
+        transform: 'translate(' + (width / 2) + ',0)',
       })
       .call(yAxis);
 
-    console.log(this.frame, this.xLimits, this.yLimits, width, height);
     return this;
+  };
+
+  Plotter.prototype.setListeners = function(e) {
+
+
+    var
+      xSVGHeight = select('.X').node().getBBox().height,
+      xTopDistance = $('.X').offset().top-xSVGHeight,
+      ySVGWidth = select('.Y').node().getBBox().width,
+      yLeftDistance = $('.Y ').offset().left + ySVGWidth,
+      abs = Math.abs,
+      mouseCloseTo = {
+        x: false,
+        y: false
+      };
+
+    $(window).on({
+      mousemove: function(e) {
+        var cursorAxisDistance = {
+          x: abs(e.clientY - xTopDistance),
+          y: abs(e.clientX - yLeftDistance)
+        };
+        if (cursorAxisDistance.x < 6) {
+          mouseCloseTo.x = true;
+          $('svg').css('cursor','ew-resize');
+        } else if (cursorAxisDistance.y < 6) {
+          mouseCloseTo.y = true;
+          $('svg').css('cursor','ns-resize');
+        } else{
+          if(mouseCloseTo.x || mouseCloseTo.y){
+            mouseCloseTo = {
+              x: false,
+              y: false
+            };
+            $('svg').css('cursor','default');
+          }
+        }
+
+      },
+      mousedown: function(e) {
+
+      }
+    });
+
   };
 
 
